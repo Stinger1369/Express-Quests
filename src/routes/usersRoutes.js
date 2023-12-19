307de337da4df24ed25de264c0c28bcb1703a2fa
bilel zaaraoui <72788978+Stinger1369@users.noreply.github.com>
2023-12-19 16:22:41 +0100

    2023-12-19-16h17

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,14 +4,41 @@ const database = require('../../config/database'); // Assurez-vous que le chemin
 
 // Route GET /api/users - Renvoie tous les utilisateurs
 router.get('/api/users', async (req, res) => {
-  try {
-    const [users] = await database.query('SELECT * FROM users');
-    res.json(users);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erreur serveur');
-  }
-});
+    try {
+      // Récupération des paramètres de la requête
+      const { language, city } = req.query;
+  
+      // Construction de la requête SQL de base
+      let sql = 'SELECT * FROM users';
+      const params = [];
+  
+      // Ajout des conditions en fonction des paramètres
+      if (language || city) {
+        sql += ' WHERE';
+  
+        if (language) {
+          sql += ' language = ?';
+          params.push(language);
+        }
+  
+        if (city) {
+          if (language) {
+            sql += ' AND';
+          }
+          sql += ' city = ?';
+          params.push(city);
+        }
+      }
+  
+      // Exécution de la requête
+      const [users] = await database.query(sql, params);
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erreur serveur');
+    }
+  });
+  
 
 // Route GET /api/users/:id - Renvoie un utilisateur spécifique
 router.get('/api/users/:id', async (req, res) => {
