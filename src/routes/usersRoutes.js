40b2a657116efb37b16227aee6288edf15a139d9
bilel zaaraoui <72788978+Stinger1369@users.noreply.github.com>
2023-12-20 09:16:07 +0100

    2023-12-20-09h14
    post

---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const database = require('../../config/database'); // Assurez-vous que le chemin est correct
+
+// Route GET /api/users - Renvoie tous les utilisateurs
+router.get('/api/users', async (req, res) => {
+    try {
+      // Récupération des paramètres de la requête
+      const { language, city } = req.query;
+  
+      // Construction de la requête SQL de base
+      let sql = 'SELECT * FROM users';
+      const params = [];
+  
+      // Ajout des conditions en fonction des paramètres
+      if (language || city) {
+        sql += ' WHERE';
+  
+        if (language) {
+          sql += ' language = ?';
+          params.push(language);
+        }
+  
+        if (city) {
+          if (language) {
+            sql += ' AND';
+          }
+          sql += ' city = ?';
+          params.push(city);
+        }
+      }
+  
+      // Exécution de la requête
+      const [users] = await database.query(sql, params);
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erreur serveur');
+    }
+  });
+  
+
+// Route GET /api/users/:id - Renvoie un utilisateur spécifique
+router.get('/api/users/:id', async (req, res) => {
+  try {
+    const [users] = await database.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+    if (users.length === 0) {
+      return res.status(404).send('Utilisateur non trouvé');
+    }
+    res.json(users[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur serveur');
+  }
+});
+
+
+router.post('/api/users', async (req, res) => {
+  try {
+      const { firstname, lastname, email, city, language } = req.body;
+
+      // Validation des données (à implémenter)
+
+      // Requête SQL pour insérer l'utilisateur
+      const sql = 'INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)';
+      await database.query(sql, [firstname, lastname, email, city, language]);
+
+      res.status(201).send('Utilisateur créé avec succès');
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Erreur serveur');
+  }
+});
+module.exports = router;
